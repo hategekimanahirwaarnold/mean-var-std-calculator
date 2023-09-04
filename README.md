7aa88f2d767c4c607866599fe55dccdb185afa0d
Hirwa Arnold <macbook@macbooks-MacBook-Pro.local>
2023-09-04 23:04:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mean-Variance-Standard Deviation Calculator
+
+This is the Mean-Variance-Standard Deviation Calculator project. Instructions for building this project can be found at https://www.freecodecamp.org/learn/data-analysis-with-python/data-analysis-with-python-projects/mean-variance-standard-deviation-calculator
---
+++ mean_var_std.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def calculate(list):
+    if len(list) < 9:
+        raise ValueError("List must contain nine numbers.")
+    else: 
+        npList = np.array(list)
+        matrix = npList.reshape(3,3)
+        response = {
+                'mean': [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.mean().tolist()],
+                'variance': [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.var().tolist()],
+                'standard deviation': [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.std().tolist()],
+                'max': [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.max().tolist()],
+                'min': [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.min().tolist()],
+                'sum': [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.sum().tolist()]
+            }
+
+        return response
